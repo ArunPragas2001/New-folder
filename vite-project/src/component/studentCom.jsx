8048ee47533c03c79fa50da2d9dab74fa21b2e85
ArunPragas2001 <pragas1@gmail.com>
2026-09-11 12:11:55 +0530

    update package.json and studentCom.jsx

--- vite-project/src/component/studentCom.jsx
+++ vite-project/src/component/studentCom.jsx
@@ -1,9 +1,30 @@
-export default function StudentCom(props){
+import { useState } from "react";
+/*export default function StudentCom(props){
 return(
     <div>
     <h1>Student Information </h1>
     <p>Name:{props.name}</p>    
     <p>Course:{props.course}</p>
     </div>
-);
-}
+);*/
+
+export default function StudentCom({name,course,age}){
+    const[studentName,setStudentName]=useState("");
+    
+    const showName(){
+        setStudentName
+    }
+
+
+
+
+    return(
+        <div>
+        <h1>Student Information </h1>
+        <p>Name:{props.name}</p>    
+        <p>Course:{props.course}</p>
+        </div>
+    );
+
+
+
